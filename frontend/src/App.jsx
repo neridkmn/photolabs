c17b7aa454e2d2e47fb5e9469bfbbb1479bb517e
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-17 16:17:55 -0400

    Delete TopNavigationBar and PhotoList components from App.jsx. Add and import HomeRoute component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,17 @@
 import React from 'react';
+import HomeRoute from 'routes/HomeRoute';
 
-import PhotoList from 'components/PhotoList';
 import './App.scss';
-import TopNavigationBar from 'components/TopNavigationBar';
+ 
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
-  // const photos = new Array(3); // creating an empty array
-
-  // const photosLength = photos.length; // for loop didn't work with photos.length, so I assigned it to a new variable. 
-
-  // for (let i = 0; i < photosLength; i++) {
-  //   photos.push(sampleDataForPhotoListItem);
-  // }
 
     return (
     <div className="App">
-      {/* {photos.map((photo, index) => //Instead of creating 3 copies of the same component, I'm looping through the data and render a component for each element.
-        <PhotoListItem
-          key={index} //assign a key using index from the .map method
-          location={photo.location}
-          imageSource={photo.imageSource}
-          username={photo.username}
-          profile={photo.profile}
-        />
-      )} */}
-      <TopNavigationBar />
-      <PhotoList />
+     <HomeRoute />
     </div>
   );
 };
