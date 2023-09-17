b5642dcbbd8b2fa9d96522749fe9c5865ac20eab
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-17 17:17:04 -0400

    Add functions to add and remove photos.

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -8,6 +8,13 @@ const HomeRoute = (props) => {
   const { photos, topics } = props; 
   const [ favPhotos, setFavPhotos ] = useState([]);
 
+  const addToFavPhotos = (photoToAdd) => { 
+    setFavPhotos([...favPhotos, photoToAdd]) // Take existing favorite photos, and add the new favorited photo to the array.
+  }
+  const removeFromFavPhotos = (photoToRemove) => {
+    setFavPhotos(photos.filter((item) => item.id !== photoToRemove.id)) // filter out the photo that was unfavorited.
+  }
+
   return (
     <div className="home-route">
       <TopNavigationBar topics={topics}/>
