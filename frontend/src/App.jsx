eb5624ac9d0291f21f22c55850f73a48d9dcef92
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-17 15:09:30 -0400

    Remove the new array and PhotoListItem component from App.jsx. Import and add PhotoList component to it.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,24 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+// import PhotoListItem from './components/PhotoListItem';
+import PhotoList from 'components/PhotoList';
 import './App.scss';
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
-  const photos = new Array(3); // creating an empty array
+  // const photos = new Array(3); // creating an empty array
 
-  const photosLength = photos.length; // for loop didn't work with photos.length, so I assigned it to a new variable. 
+  // const photosLength = photos.length; // for loop didn't work with photos.length, so I assigned it to a new variable. 
 
-  for (let i = 0; i < photosLength; i++) {
-    photos.push(sampleDataForPhotoListItem);
-  }
+  // for (let i = 0; i < photosLength; i++) {
+  //   photos.push(sampleDataForPhotoListItem);
+  // }
 
     return (
     <div className="App">
-      {photos.map((photo, index) => //Instead of creating 3 copies of the same component, I'm looping through the data and render a component for each element.
+      {/* {photos.map((photo, index) => //Instead of creating 3 copies of the same component, I'm looping through the data and render a component for each element.
         <PhotoListItem
           key={index} //assign a key using index from the .map method
           location={photo.location}
@@ -36,7 +26,8 @@ const App = () => {
           username={photo.username}
           profile={photo.profile}
         />
-      )}
+      )} */}
+      <PhotoList />
     </div>
   );
 };
