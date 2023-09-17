eae15a2bc18f056a77365100ab6061d3e4d64eaa
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-16 20:49:10 -0400

    Implement FavIcon functionality

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,18 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+
+  const [selected, setSelected] = useState(false); //Create selected state (Boolean)
+
+  const handleOnClick = () => { // function to toggle selected state
+    setSelected(!selected);
+  };
+
+  //Add selected as a prop to FavIcon component and onClick to its div tag.
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleOnClick}>
       <div className="photo-list__fav-icon-svg">
-        <FavIcon /> 
+        <FavIcon selected={selected}/> 
       </div>
     </div>
   );
