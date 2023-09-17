ba4b323c7a7ff1bc3c238e4595add22d0ecd3a1f
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-17 17:05:14 -0400

    Import useState and declare it.

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,12 @@
 import TopNavigationBar from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = (props) => {
   const { photos, topics } = props; 
+  const [ favPhotos, setFavPhotos ] = useState([]);
 
   return (
     <div className="home-route">
