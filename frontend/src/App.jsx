aced45b15810b97abe1e61c7d3398f1593776711
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-17 16:06:10 -0400

    Remove TopicList component from App.jsx. Add and import TopNavigationBar component to the file.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import PhotoList from 'components/PhotoList';
 import './App.scss';
-import TopicList from 'components/TopicList';
+import TopNavigationBar from 'components/TopNavigationBar';
 
 
 // Note: Rendering a single component to build components in isolation
@@ -27,7 +27,7 @@ const App = () => {
           profile={photo.profile}
         />
       )} */}
-      <TopicList />
+      <TopNavigationBar />
       <PhotoList />
     </div>
   );
