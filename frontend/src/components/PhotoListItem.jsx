eda92d9402c2ab9fc7d3c3caf045933ca9c3d252
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-17 17:49:47 -0400

    Get the entire photo object as a prop and use photo object's fields.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,20 +5,21 @@ import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
 
-  const { location, imageSource, username, profile, addToFavPhotos, removeFromFavPhotos } = props; 
+  const { photo, addToFavPhotos, removeFromFavPhotos } = props; // get the entire photo object as a prop.
 
   return(
     <div className="photo-list__item">
       <PhotoFavButton 
         addToFavPhotos={addToFavPhotos} 
         removeFromFavPhotos={removeFromFavPhotos} 
+        photo={photo} // pass down the photo object as prop
       />
-      <img className="photo-list__image" src={imageSource} />
+      <img className="photo-list__image" src={photo.urls.regular} /> {/** use photo object's fields */}
       <div className="photo-list__user-details">
-       <img className="photo-list__user-profile" src={profile} />
+       <img className="photo-list__user-profile" src={photo.user.profile} /> {/** use photo object's fields */}
        <div className="photo-list__user-info">
-        <div>{username}</div>
-        <div className="photo-list__user-location">{location.city}, {location.country}</div>
+        <div>{photo.user.name}</div> {/** use photo object's fields */}
+        <div className="photo-list__user-location">{photo.location.city}, {photo.location.country}</div> {/** use photo object's fields */}
        </div>
       </div>
     </div>
