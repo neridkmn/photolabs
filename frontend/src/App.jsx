a80fad2c85d71c5916ecb7b9d8f10509fc74711d
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-17 16:40:26 -0400

    Import mock photos and topics and add them as props to HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import HomeRoute from 'routes/HomeRoute';
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 
 import './App.scss';
  
@@ -11,7 +13,7 @@ const App = () => {
 
     return (
     <div className="App">
-     <HomeRoute />
+     <HomeRoute photos={photos} topics={topics} />
     </div>
   );
 };
