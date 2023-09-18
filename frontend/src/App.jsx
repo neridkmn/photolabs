faccaebb8115cb7c159a2521f6d40331bffd1f03
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-18 18:16:58 -0400

    Add show modal state and conditional rendering.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HomeRoute from 'routes/HomeRoute';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
@@ -11,11 +11,12 @@ import './App.scss';
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
+    const [showModal, setShowModal] = useState(false); //Create a state to determine to show the modal or not. 
 
     return (
     <div className="App">
      <HomeRoute photos={photos} topics={topics} />
-     <PhotoDetailsModal />
+     {showModal && <PhotoDetailsModal />} {/**conditional rendering to show modal */}
     </div>
   );
 };
