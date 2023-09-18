62aea8593394891c469a9d898f10c0b71e3098db
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-18 18:25:21 -0400

    Extract and pass down setShowModal function.

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = (props) => {
-  const { photos, topics } = props; 
+  const { photos, topics, setShowModal } = props; 
   const [ favPhotos, setFavPhotos ] = useState([]);
 
   const addToFavPhotos = (photoToAdd) => { 
@@ -18,7 +18,11 @@ const HomeRoute = (props) => {
   return (
     <div className="home-route">
       <TopNavigationBar topics={topics} isFavPhotoExist={favPhotos.length > 0 ? true: false}/> {/** determine if any photo is favorited and pass down to the top nagivation bar. */}
-      <PhotoList photos={photos} addToFavPhotos={addToFavPhotos} removeFromFavPhotos={removeFromFavPhotos}/>
+      <PhotoList photos={photos} 
+      addToFavPhotos={addToFavPhotos} 
+      removeFromFavPhotos={removeFromFavPhotos}
+      setShowModal={setShowModal}
+      />
     </div>
   );
 };
