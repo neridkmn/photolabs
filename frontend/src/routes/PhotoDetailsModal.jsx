3fd01182511193a4e7704db3de1c24a6b8d1d93c
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-18 18:34:30 -0400

    Declare handleOnClose function and call setShowModal with false parameter to close the modal. Add onclick to button in return.

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = (props) => {
+  const { setShowModal } =props;
+  const handleOnClose = () => {setShowModal(false)}; //Declare handleOnClose function to close the modal
+
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
+        <img src={closeSymbol} alt="close symbol" onClick={handleOnClose}/>
       </button>
     </div>
   )
