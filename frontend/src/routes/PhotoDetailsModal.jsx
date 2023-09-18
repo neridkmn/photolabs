bc20d4ba185236e026c66a209bb22489c0342763
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-18 18:45:41 -0400

    Extract selectedPhoto as prop and print it to the console.

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,9 +4,10 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
-  const { setShowModal } =props;
+  const { setShowModal, selectedPhoto } =props;
   const handleOnClose = () => {setShowModal(false)}; //Declare handleOnClose function to close the modal
 
+  console.log(selectedPhoto)
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button">
