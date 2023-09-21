46d17da2ecdacef0d40686264be0a7f423adc9c2
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-21 15:14:59 -0400

    Update App.jsx with useApplicationData function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,25 @@
 import React, { useState } from 'react';
 import HomeRoute from 'routes/HomeRoute';
-import photos from 'mocks/photos';
-import topics from 'mocks/topics';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
-
+import useApplicationData from 'hooks/useApplicationData';
 import './App.scss';
  
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-
-    const [showModal, setShowModal] = useState(false); //Create a state to determine to show the modal or not. 
-    const [selectedPhoto, setSelectedPhoto] = useState({}); //Create a state to show selected photo details in the console. 
-
-    const [ favPhotos, setFavPhotos ] = useState([]);
-
-    const addToFavPhotos = (photoToAdd) => { 
-      setFavPhotos([...favPhotos, photoToAdd]) // Take existing favorite photos, and add the new favorited photo to the array.
-    }
-    const removeFromFavPhotos = (photoToRemove) => {
-      setFavPhotos(favPhotos.filter((item) => item.id !== photoToRemove.id)) // filter out the photo that was unfavorited.
-    }
+  const {
+    photos,
+    topics,
+    showModal,
+    setShowModal,
+    selectedPhoto,
+    setSelectedPhoto,
+    addToFavPhotos,
+    removeFromFavPhotos,
+    favPhotos
+  } = useApplicationData();
+  
 
     return (
     <div className="App">
