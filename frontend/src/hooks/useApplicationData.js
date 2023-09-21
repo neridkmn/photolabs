6a127b2097baa99c3f3073dc42b2cae0ddba8a3e
Neriman Dikmen <sanerneriman@gmail.com>
2023-09-21 16:18:07 -0400

    Update the function with useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,33 +1,80 @@
-import React, { useState } from 'react';
+import { useReducer } from 'react';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 
-const useApplicationData = () => {
-  const [showModal, setShowModal] = useState(false); //Create a state to determine to show the modal or not. 
-  const [selectedPhoto, setSelectedPhoto] = useState({}); //Create a state to show selected photo details in the console. 
-
-  const [favPhotos, setFavPhotos] = useState([]);
-
-  const addToFavPhotos = (photoToAdd) => {
-    setFavPhotos([...favPhotos, photoToAdd]); // Take existing favorite photos, and add the new favorited photo to the array.
-  };
-  const removeFromFavPhotos = (photoToRemove) => {
-    setFavPhotos(favPhotos.filter((item) => item.id !== photoToRemove.id)); // filter out the photo that was unfavorited.
-  };
-
-
-return {
-  photos,
-  topics,
-  showModal,
-  setShowModal,
-  selectedPhoto,
-  setSelectedPhoto,
-  addToFavPhotos,
-  removeFromFavPhotos,
-  favPhotos
+const ACTIONS = {
+  SHOW_MODAL: 'SHOW_MODAL',
+  SET_SELECTED_PHOTO: 'SET_SELECTED_PHOTO',
+  ADD_TO_FAV_PHOTOS: 'ADD_TO_FAV_PHOTOS',
+  REMOVE_FROM_FAV_PHOTOS: 'REMOVE_FROM_FAV_PHOTOS'
 }
 
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.SHOW_MODAL:
+      return {
+        ...state,
+        showModal: action.payload
+      }
+    case ACTIONS.SET_SELECTED_PHOTO:
+      return {
+        ...state,
+        selectedPhoto: action.payload
+      }
+    case ACTIONS.ADD_TO_FAV_PHOTOS:
+      return {
+        ...state,
+        favPhotos: [...state.favPhotos, action.payload]
+      }
+    case ACTIONS.REMOVE_FROM_FAV_PHOTOS:
+      return {
+        ...state,
+        favPhotos: state.favPhotos.filter((item) => item.id !== action.payload.id)
+      }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+const useApplicationData = () => {
+
+  const [state, dispatch] = useReducer(reducer, {
+    showModal: false,
+    selectedPhoto: {},
+    favPhotos: [],
+  });
+
+  const setShowModal = (payload) => {
+    dispatch({ type: ACTIONS.SHOW_MODAL, payload: payload })
+  }
+
+  const setSelectedPhoto = (photo) => {
+    dispatch({ type: ACTIONS.SET_SELECTED_PHOTO, payload: photo})
+  }
+
+  const addToFavPhotos = (photo) => {
+    dispatch({type: ACTIONS.ADD_TO_FAV_PHOTOS, payload: photo})
+  }
+
+  const removeFromFavPhotos = (photo) => {
+    dispatch({type: ACTIONS.REMOVE_FROM_FAV_PHOTOS, payload: photo})
+  }
+
+  const data = {
+    topics,
+    photos
+  }
+
+  return {
+    state,
+    data,
+    setShowModal,
+    setSelectedPhoto,
+    addToFavPhotos,
+    removeFromFavPhotos,
+  }
 };
 
 export default useApplicationData;
